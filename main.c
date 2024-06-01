be68cac3e7bac0eb608edb60ccdebe28bee51ebb
Lucia <luciacastagninoo@gmail.com>
2024-05-31 22:57:29 -0300

    prueba

--- main.c
+++ main.c
@@ -15,13 +15,14 @@ int main()
     int op;
 
     printf("Ingrese con que area va a trabajar\n\n");
-    printf("1. Empleados\n");
-    printf("2. Viajes\n");
-    printf("3. Ventas\n");
-    printf("4. Clientes\n");
+    printf("1. Empleados.\n");
+    printf("2. Viajes.\n");
+    printf("3. Ventas.\n");
+    printf("4. Clientes.\n");
+    printf("5. Salir.\n");
     fflush(stdin);
     scanf("%d", &op);
-
+    do{
     switch(op)
     {
     case 1:
@@ -44,7 +45,9 @@ int main()
             menuClientes();
             break;
         }
-    }
+    case 5:
+        printf("\nSaliendo del sistema...\n");
+    }}while(op!=5);
 
     return 0;
 }
